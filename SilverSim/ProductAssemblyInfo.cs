1703712b9ccad22ffde755addba5438195cd4ab9
ft <ft@noemail>
2015-09-21 18:08:42 +0200

    added build environment files

---
+++ SilverSim/ProductAssemblyInfo.cs
@@ -0,0 +1,15 @@
+// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using System.Reflection;
+
+[assembly: AssemblyProduct("SilverSim/Arriba")]
+[assembly: AssemblyCompany("SilverSim Development")]
+[assembly: AssemblyCopyright("Affero GPLv3 License + Link Exception")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: SilverSim.Types.Assembly.InterfaceVersion("1.0.0.0")]
